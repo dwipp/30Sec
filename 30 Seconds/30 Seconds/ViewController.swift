739412f5f152ef51da500d7770bea3b3d3b4fbc6
Dwi Permana Putra <dwi.putra@icloud.com>
2020-09-16 21:17:29 +0800

    First bump

---
+++ 30 Seconds/30 Seconds/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  30 Seconds
+//
+//  Created by Dwi Putra on 16/09/20.
+//  Copyright © 2020 Dwi Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
